45fdacd6b0909108e63946c9d43fc302a897bf4a
Walid Bellili <walid.bellili.seixas@gmail.com>
2023-01-02 11:58:09 +0100

    components cutted first style component for body

---
+++ src/components/Li.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Li = ({text}) => {
+  return (
+    <li>{text}</li>
+  )
+}
+
+export default Li
---
+++ src/styles/color.js
@@ -0,0 +1,4 @@
+export const blue = '#0A192F'
+export const green = '0A192F'
+export const white = 'CCD6F6'
+export const grey = '8892B0'
